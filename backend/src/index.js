595dae7c2cf6bfb23f94f567aaf98220c8f4cb1f
Rajeev1706 <rajeevsidharth17@gmail.com>
2026-09-07 18:12:23 +0530

    new features

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,8 +22,11 @@ const PORT = process.env.PORT || 3001
 
 // ── Middleware ────────────────────────────────────────────────────────────────
 app.use(cors({
-  origin:      process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://pes-park-opal.vercel.app"
+  ],
+  credentials: true
 }))
 app.use(express.json({ limit: "15mb" }))
 
